2ed377dbffb96d86d722ae486f3a8ced808378da
Meet Maheshwari <145050800+Meet-Maheshwari@users.noreply.github.com>
2025-06-18 18:28:38 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://mypharma-backend-sigma.vercel.app",
+  baseURL: "https://mypharma-backend-sigma.vercel.app/api",
   withCredentials: true,
 });
